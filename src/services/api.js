49f5c0e6bcb1f2425004c3ccd3ac84340d4f136a
{ayan} <{ayandavian07@gmail.com}>
2026-05-15 01:08:28 +0530

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://elm-backend-hmfydpb4gzgggmec.uaenorth-01.azurewebsites.net/api';
 
 // Create axios instance (don't set token here - let AuthContext handle it)
 const api = axios.create({
